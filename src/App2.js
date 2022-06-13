af6739a6595572992f829068865382843228da83
ray letoa <ray.letoa@gmail.com>
2022-06-13 15:08:12 +1200

    deleted most lines

--- src/App2.js
+++ src/App2.js
@@ -4,22 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <h1>David was here!</h1>
-
-        LINE 8 CHANGE THIS IS GONNA BE MESSY
-        <h1>David was here!</h1>
-
-        LINE 8 CHANGE THIS IS GONNA BE MESSY
-        <h1>David was here!</h1>
-
-        LINE 8 CHANGE THIS IS GONNA BE MESSY
-        <h1>David was here!</h1>
-
-        LINE 8 CHANGE THIS IS GONNA BE MESSY
-
-      </header>
+     
     </div>
   );
 }
