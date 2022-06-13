521388b6b3307ded77fb13904c4148bdb1526ff5
ray letoa <ray.letoa@gmail.com>
2022-06-13 15:07:21 +1200

    new apps

---
+++ src/App2.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>David was here!</h1>
+
+        LINE 8 CHANGE THIS IS GONNA BE MESSY
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
